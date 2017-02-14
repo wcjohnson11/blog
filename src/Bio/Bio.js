7d4a6f37a7c7391e4475fc8392582831e84fbdae
wcjohnson11 <williamcouperjohnson@gmail.com>
2017-02-13 18:07:58 -0800

    refine styles

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -9,8 +9,6 @@ class Bio extends Component {
           <p>Analytics, Art, Data, Engineering</p>
           <p>Currently at <a href="https://mindmakersproject.org" target="_blank">Mind Makers</a>, formerly at <a href="https://www.segment.com" target="_blank">Segment</a></p>
           <ul>
-            <li><a href="/projects" className="project-icon"><i className="fa fa-code"></i></a></li>
-            <li><a href="/blog"><i className="fa fa-pencil"></i></a></li>
             <li><a href="https://github.com/wcjohnson11" target="blank"><i className="fa fa-github-alt"></i></a></li>
             <li><a href="https://twitter.com/wcj111" target="blank"><i className="fa fa-twitter"></i></a></li>
             <li><a href="https://www.linkedin.com/in/wcj11" target="blank"><i className="fa fa-linkedin"></i></a></li>
